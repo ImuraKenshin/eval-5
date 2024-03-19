5c9d797998859c43109131701b3eba63cd8a6e07
ImuraKenshin <161824604+ImuraKenshin@users.noreply.github.com>
2024-03-19 12:39:44 +0100

    update 19/03 12h30

--- app.js
+++ app.js
@@ -193,20 +193,3 @@ monform.addEventListener("submit",(e)=>{
         monform.submit()
     }
 })
-
-
-
-/*
-***********************Map**************************
-*/
-let map = L.map('map').setView([45.439695, 4.3871779],17)
-
-
-// des cartes prisent sur leaflet et leaflet extra
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-var marker = L.marker([45.439695, 4.3871779]).addTo(map);
---
+++ map.js
@@ -0,0 +1,14 @@
+/*
+***********************Map**************************
+*/
+let map = L.map('map').setView([45.439695, 4.3871779],17)
+
+
+// des cartes prisent sur leaflet et leaflet extra
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([45.439695, 4.3871779]).addTo(map);
