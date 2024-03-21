b7237535a9bb7211f80273f6b7c7af999a1b763c
ImuraKenshin <161824604+ImuraKenshin@users.noreply.github.com>
2024-03-21 14:52:09 +0100

    update 21/03 15h

---
+++ favIcone.js
@@ -0,0 +1,23 @@
+
+
+// light recupère l'information si le thème est sombre
+
+let dark = window.matchMedia('(prefers-color-scheme:dark)')
+
+// la fonction affiche le favIcone en fonction de la valeur de light.matches
+
+function favIcone(){
+    let link = document.getElementById("favIcone")
+    // si le thème est sombre
+    if(dark.matches){
+        // affiche le favIcone blanc dans link
+        link.href = "./imagesSIte/fav-icone.png"
+    }else{
+        //sinon
+        // affiche le favicone noir dans link
+        link.href = "./imagesSIte/favicon-noir.png"
+    }
+}
+
+// j'appel ma fonction
+favIcone()
